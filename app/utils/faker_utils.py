defbfbc27b8c24ffdd20dc91a99386051a415199
Pablo <juansean527@gmail.com>
2025-12-15 11:29:38 -0500

    se modifican los faker_utils para evitar confilictos com espacios y puntuaciones en el list personas

--- app/utils/faker_utils.py
+++ app/utils/faker_utils.py
@@ -1,9 +1,17 @@
 import random
+import unicodedata
 from faker import Faker
 from typing import List, Dict, Any
 
 fake = Faker('es_ES')
 DOMINIOS_REALES = ['gmail.com', 'hotmail.com', 'outlook.com', 'yahoo.com']
+
+def normalizar(texto: str) -> str:
+    # quitar acentos y pasar a ascii simple
+    nfkd = unicodedata.normalize("NFKD", texto)
+    solo_ascii = "".join(c for c in nfkd if not unicodedata.combining(c))
+    # quitar espacios y pasar a minúsculas
+    return solo_ascii.replace(" ", "").lower()
 
 def generar_personas(cantidad: int) -> List[Dict[str, Any]]:
     """Genera personas para inserción masiva."""
@@ -15,10 +23,13 @@ def generar_personas(cantidad: int) -> List[Dict[str, Any]]:
         firstname = fake.first_name()
         lastname = fake.last_name()
         dominio = random.choice(DOMINIOS_REALES)
+
+        user = f"{normalizar(firstname)}.{normalizar(lastname)}"
+        email = f"{user}@{dominio}"
         personas.append({
             "firstname": firstname,
             "lastname": lastname,
-            "email": f"{firstname.lower()}.{lastname.lower()}@{dominio}",
+            "email": email,
             "phone": fake.phone_number()[:15],
             "birthdate": fake.date_of_birth(minimum_age=18, maximum_age=80).strftime('%Y-%m-%d'),
             "isactive": random.choice([True, False]),
